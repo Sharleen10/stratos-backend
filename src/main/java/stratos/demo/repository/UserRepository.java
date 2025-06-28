6c46d1e70aad50832786124931937871e4cf8f06
Sharleen10 <sharleensshabangu@gmail.com>
2025-06-28 20:25:20 +0200

    feat(repository): create UserRepository extending JpaRepository with findByEmail and existsByEmail methods

---
+++ src/main/java/stratos/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package stratos.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import stratos.demo.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
