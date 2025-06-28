d996f6291c51e19f86d429cb88340f478dee7261
Sharleen10 <sharleensshabangu@gmail.com>
2025-06-28 10:06:57 +0200

    feat(model): create Role entity including RoleType enumeration

---
+++ src/main/java/stratos/demo/model/Role.java
@@ -0,0 +1,26 @@
+package stratos.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType name;
+
+    public enum RoleType {
+        ROLE_MANAGER,
+        ROLE_TEAM_MEMBER
+    }
+}
