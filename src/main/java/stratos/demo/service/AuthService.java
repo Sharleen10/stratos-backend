21b973fa15b6e56cbb9a12d0102bf704672cf03b
Sharleen10 <sharleensshabangu@gmail.com>
2025-06-30 11:03:01 +0200

    feat(service): add AuthService for user registration and authentication

---
+++ src/main/java/stratos/demo/service/AuthService.java
@@ -0,0 +1,55 @@
+package stratos.demo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import stratos.demo.config.JwtService;
+import stratos.demo.dto.AuthResponse;
+import stratos.demo.dto.RegisterRequest;
+import stratos.demo.model.Role;
+import stratos.demo.model.User;
+import stratos.demo.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .fullName(request.getFullName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.RoleType.valueOf("ROLE_" + request.getRole().toUpperCase()))
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
