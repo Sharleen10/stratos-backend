8e6585587f7ce0b2338f24bcc0fe33c98afe99dd
Sharleen10 <sharleensshabangu@gmail.com>
2025-06-30 10:56:18 +0200

    feat(dto): create RegisterRequest DTO to capture registration input fields including role selection

---
+++ src/main/java/stratos/demo/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package stratos.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String fullName;
+    private String email;
+    private String password;
+    private String role; // "manager" or "team_member"
+}
